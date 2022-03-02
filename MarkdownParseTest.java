b9a77f488f5d147fe91b7eedc93d378c493ac542
mialyssa <97639434+mialyssa@users.noreply.github.com>
2022-03-02 15:13:33 -0800

    Lab report 4 snippets

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -78,4 +78,26 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
 
+    //Lab 8 2/25/22
+    @Test
+    public void testSnippet1() throws IOException {
+        String contents = Files.readString(Path.of("./snippet1.md"));
+        List<String> expect = List.of("google.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        String contents = Files.readString(Path.of("./snippet2.md"));
+        List<String> expect = List.of("a.com, a.com(()), example.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        String contents = Files.readString(Path.of("./snippet3.md"));
+        List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
 }
